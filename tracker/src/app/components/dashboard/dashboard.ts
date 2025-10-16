83e306177a8659e8836e27c287c6ca74bb783668
Riccardo Calabri <153217211+777riki@users.noreply.github.com>
2025-10-16 07:17:36 +0000

    feat(dashboard): aggiunta integrazione di chart.js per visualizzazione grafici dei prezzi delle criptovalute

--- tracker/src/app/components/dashboard/dashboard.ts
+++ tracker/src/app/components/dashboard/dashboard.ts
@@ -16,8 +16,6 @@ export class Dashboard implements OnInit {
   coinService: Coins = inject(Coins);
   coins: WritableSignal<Coin[]> = signal<Coin[]>([]);
 
-
-
   ngOnInit(): void {
     this.coinService.getTrendingCoins()
       .subscribe(data => {
@@ -26,6 +24,7 @@ export class Dashboard implements OnInit {
 
         setTimeout(() => {
           this.coins().forEach((coin, i) => {
+
             const ctx = document.getElementById(`myChart-${i}`) as HTMLCanvasElement;
 
             const price_now = parseFloat(coin.price_usd);
@@ -36,6 +35,17 @@ export class Dashboard implements OnInit {
             const price_7d_ago = price_now / (1 + percent_7d / 100);
             const price_24h_ago = price_now / (1 + percent_24h / 100);
             const price_1h_ago = price_now / (1 + percent_1h / 100);
+
+            let borderColor = '';
+            let backgroundColor = '';
+
+            if (price_7d_ago > price_now) {
+              borderColor = '#D50000';
+              backgroundColor = 'rgba(213, 0, 0, 0.1)';
+            } else {
+              borderColor = '#00C853';
+              backgroundColor = 'rgba(0, 200, 83, 0.1)';
+            }
 
             if (ctx) {
               new Chart(ctx, {
@@ -51,15 +61,17 @@ export class Dashboard implements OnInit {
                       price_now
                     ],
                     borderWidth: 1,
-                    borderColor: 'blue',
-                    backgroundColor: 'rgba(0, 0, 255, 0.1)',
-                    tension: 0.3
+                    borderColor: borderColor,
+                    backgroundColor: backgroundColor,
+                    fill: true,
+                    tension: 0.4
                   }]
                 }
               });
             }
+
           });
-        });
+        }, 1);
       });
   }
 
